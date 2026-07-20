212eaaee42e78f61d66d8f6ecdd65dfb3f3ce227
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2026-07-21 01:26:17 +0600

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Quantum Code Labs",
+  description: "Quantum Code Labs is a place to learn quantum computing and coding.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <div>
+          <Header/>
+            {children}
+          <Footer/>
+        </div>
+      </body>
+    </html>
+  );
+}
